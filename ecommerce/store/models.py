3ec45f1fef7c8df74519fd71f8ccb32eba401c29
Rafi <rafitanjibulhasan@gmail.com>
2022-01-04 16:22:09 +0600

    add cart

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default = False, null=True, blank=True)
-    image = models.ImageField(null=True,blank=True)
+    image = models.ImageField(default="placeholder.png", null=True,blank=True)
     def __str__(self):
         return self.name
 
@@ -28,6 +28,27 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+  
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
     product  = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
@@ -36,6 +57,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product.name
+
+    @property
+    def get_total(self):
+        total = self.product.price*self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -48,4 +74,3 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
-
