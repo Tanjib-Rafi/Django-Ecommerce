310ac20e9a2163a03f53201345b1d67e602cb9e2
Rafi <rafitanjibulhasan@gmail.com>
2022-01-26 03:00:12 +0600

    order added

--- ecommerce/store/utils.py
+++ ecommerce/store/utils.py
@@ -42,3 +42,49 @@ def cookieCart(request):
                except:
                     pass
         return{'cartItems' : cartItems, 'order' : order, 'items' : items}
+
+def cartData(request):
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer = customer , completed = False)
+          items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
+     else:
+          cookieData = cookieCart(request)
+          cartItems = cookieData['cartItems']
+          order = cookieData['order']
+          items = cookieData['items']
+
+     return{'cartItems' : cartItems, 'order' : order, 'items' : items}
+
+def guestOrder(request,data):
+     print('user is not logged in ')
+
+     name = data['form']['name']
+     email = data['form']['email']
+
+     cookieData = cookieCart(request)
+     items = cookieData['items']
+
+     customer, created = Customer.objects.get_or_create(
+          email = email,
+     )
+     customer.name  = name
+     customer.save()
+
+     order = Order.objects.create(
+          customer = customer,
+          complete = False,
+     )
+
+     for item in items :
+          product = Product.objectsg.get(id=item['product']['id'])
+
+          orderItem = OrderItem.objects.create(
+               product=product,
+               order = order,
+               quantity = item['quantity']
+          )
+
+     return customer,order
+          
